e3f73f5b3937a6bbc23a7c0d41f303bb427f7c9b
Sai Nikhil Podduturi <sainikhilpodduturi4531@gmail.com>
2023-11-04 11:45:33 +0530

    Added Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-scroll'
+import '../styles/navbar.css'
+
+const Navbar = () => {
+  return (
+    <>
+     <nav className='navbar'>
+        <div className='navbar-menu'>
+            <Link className='navbar-menu-item'>Home</Link>
+            <Link className='navbar-menu-item'>Experience</Link>
+            <Link className='navbar-menu-item'>Projects</Link>
+            <Link className='navbar-menu-item'>Education</Link>
+        </div>
+        <button className='navbar-btn'>Contact Me</button>
+     </nav>
+    </>
+  )
+}
+
+export default Navbar
